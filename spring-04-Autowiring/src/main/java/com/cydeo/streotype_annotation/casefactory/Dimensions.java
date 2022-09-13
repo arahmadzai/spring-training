1593f58cb9b0e6f65b40dafdbd1eb515d0e59535
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-09-13 11:54:58 -0400

    creating bean from primitive workaround

--- spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/casefactory/Dimensions.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/casefactory/Dimensions.java
@@ -8,13 +8,25 @@ import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
-@AllArgsConstructor
+//@AllArgsConstructor
 @Component
 public class Dimensions {
 
     private int width;
     private int height;
     private int depth;
+
+    public Dimensions() {
+        this.width=10;
+        this.height=30;
+        this.depth=40;
+    }
+
+//    public Dimensions(){
+//        this.width = 10;
+//        this.height = 30;
+//        this.depth = 40;
+//    }
 
     public void pressPowerButton(){
         System.out.println("Power button pressed");
