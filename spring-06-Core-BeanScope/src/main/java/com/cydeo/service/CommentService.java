7fb46b0ff74ca2bc2599267b5e45e720d9eb7ea6
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-09-15 17:58:57 -0400

    BeanScope

---
+++ spring-06-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,30 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    //by creating the following constructor dependency injection happens we don't need to add @Autowired
+    public CommentService(CommentRepository commentRepository,@Qualifier("Email") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){
+        //save in the DB
+        //send email
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+
+
+
+    }
+}
