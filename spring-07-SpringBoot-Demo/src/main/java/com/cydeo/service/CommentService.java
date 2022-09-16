640695b2a881ef25be7ba7af2d7827e706257050
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-09-16 01:42:52 -0400

    Spring-boot

--- spring-07-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -1,5 +1,7 @@
 package com.cydeo.service;
 
+import com.cydeo.config.AppConfigData;
+import com.cydeo.config.DBConfigData;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
@@ -11,11 +13,14 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
-
+    private final AppConfigData appConfigData;
+    private final DBConfigData dbConfigData;
     //by creating the following constructor dependency injection happens we don't need to add @Autowired
-    public CommentService(CommentRepository commentRepository,@Qualifier("Email") CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository, @Qualifier("Email") CommentNotificationProxy commentNotificationProxy, AppConfigData appConfigData, DBConfigData dbConfigData) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
+        this.appConfigData = appConfigData;
+        this.dbConfigData = dbConfigData;
     }
 
     public void publishComment(Comment comment){
@@ -24,7 +29,18 @@ public class CommentService {
         commentRepository.storeComment(comment);
         commentNotificationProxy.sendComment(comment);
 
+    }
 
+    public void printConfigData() {
+        System.out.println(appConfigData.getUsername());
+        System.out.println(appConfigData.getPassword());
+        System.out.println(appConfigData.getUrl());
 
+    }
+
+    public void printDBConfigData(){
+        System.out.println(dbConfigData.getUsername());
+        System.out.println(dbConfigData.getPassword());
+        System.out.println(dbConfigData.getType());
     }
 }
