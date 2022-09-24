72c1a80a1bb73a3a1b65bc226369a5e6da8a0001
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-09-24 03:15:01 -0400

    Spring MVC Architecture Model Query parameter

---
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -0,0 +1,38 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CarController {
+
+    @RequestMapping("info")//http://localhost:8080/info?make=Lexus
+    public String carInfo(@RequestParam String make, Model model){
+
+        model.addAttribute("make", make);
+
+        return "car/car-info";
+
+    }
+
+    @RequestMapping("info2") //default http://localhost:8080/info2  or http://localhost:8080/info2?make=Lexus
+    public String carInfo2(@RequestParam(value = "make", required = false, defaultValue = "Lexus") String make, Model model){
+
+        model.addAttribute("make", make);
+
+        return "car/car-info";
+
+    }
+
+    @RequestMapping("info3")
+    public String carInfo3(@RequestParam String make,@RequestParam int year, Model model){ //http://localhost:8080/info3?make=Lexus&year=2023
+
+        model.addAttribute("make", make);
+        model.addAttribute("year", year);
+
+        return "car/car-info";
+
+    }
+}
