c8ef61a835fc66b3e92bc7eb10c0ed10f17b34ae
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-09-26 19:13:56 -0400

    Thymeleaf template engine

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,25 +3,30 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register") //localhost:8080/student/register
-    public String register(Model model){
 
-        model.addAttribute("students", DataGenerator.createStudent());
+        @RequestMapping(value = "/register",method = RequestMethod.GET) //localhost:8080/student/register
+        @GetMapping("/register")
+        public String register(Model model){
 
-        return "student/register";
-    }
+            model.addAttribute("students", DataGenerator.createStudent());
 
-    @RequestMapping("/welcome") //localhost:8080/student/welcome?name=Ozzy
-    public String welcome(){
+            return "student/register";
 
 
-        return "student/welcome";
-    }
+        }
+
+        @RequestMapping(value = "/welcome",method = RequestMethod.POST) //localhost:8080/student/welcome?name=Ozzy
+        @PostMapping("/welcome")
+        public String welcome(@RequestParam String name){
+
+            return "student/welcome";
+        }
+
+
 }
