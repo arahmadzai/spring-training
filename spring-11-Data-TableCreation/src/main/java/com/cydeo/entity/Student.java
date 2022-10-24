238a6a2e8a2c3aaa5229cf45d47a4c814cf24762
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-10-24 01:18:40 -0400

    Added Enum @Enumerated(EnumType.String) @Column(name="")

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,6 +1,11 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.Gender;
+
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "students")
@@ -9,7 +14,20 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "studentFirstName")
     private String firstName;
+    @Column(name = "studentLastName")
     private String lastName;
     private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(name = "DATE")
+    private LocalDate birthDate;
+    @Column(name = "TIME")
+    private LocalTime birthTime;
+    @Column(name = "TIMESTAMP")
+    private LocalDateTime birthDateTime;
+
 }
--- spring-11-Data-TableCreation/target/classes/data.sql
+++ spring-11-Data-TableCreation/target/classes/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO EMPLOYEES(id,name)
 VALUES(1,'Mike');
 
-INSERT INTO students(first_name, last_name,email)
+INSERT INTO students(student_first_name, student_last_name,email)
 values ('Abid','Rahimi','abid_rahimi@hotmail.com');
 
-INSERT INTO students(first_name, last_name,email)
+INSERT INTO students(student_first_name,student_last_name,email)
 values ('Mohammad','Ahmadzai','MohammadAhmadzai@Cydeo.com');
