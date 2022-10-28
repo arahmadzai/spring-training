bba22e77a9f7eacecf0fc03ebc116a1e0c335356
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-10-28 12:41:00 -0400

    Spring DATA Queries JPQL and Positional Parameter

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -46,4 +46,48 @@ public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
     @Query("SELECT e.salary from Employee e where e.email= 'amcnee1@google.es'")
     Integer retrieveEmployeeSalary();
 
+    //Not Equal
+    @Query("SELECT e from Employee e where e.salary <> ?1")
+    List<Employee> retrieveEmployeeSalaryNotEqual(int salary);
+
+    //Like / Contains / StartsWith / EndsWith
+    @Query("SELECT e from Employee e where e.firstName like ?1")
+    List<Employee> retrieveEmployeeFistNameLike(String pattern);
+
+    //Less than
+    @Query("SELECT e from Employee e where e.salary < ?1")
+    List<Employee> retrieveEmployeeSalaryLessThan(int salary);
+
+    //Greater than
+    @Query("SELECT e from Employee e where e.salary > ?1")
+    List<Employee> retrieveEmployeeSalaryGreaterThan(int salary);
+
+    //Show only first name of employee for the given salary
+    @Query("SELECT e.firstName from Employee e where e.salary > ?1")
+    List<Employee> retrieveFirstNameEmployeeSalary(int salary);
+
+    //Between
+    @Query("SELECT e.firstName from Employee e where e.salary >= ?1 AND e.salary <=?2")
+    List<Employee> retrieveFirstNameSalaryBetween(int fromSalary, int toSalary);
+
+    @Query("SELECT e from Employee e where e.salary >= ?1 AND e.salary <=?2")
+    List<Employee> retrieveSalaryBetween(int fromSalary, int toSalary);
+
+    //Before
+    @Query("select e from Employee e where e.hireDate > ?1")
+    List<Employee> retrieveEmployeeHiredBeforeDate(LocalDate date);
+
+    //Null
+    @Query("SELECT e from Employee e where e.email is null")
+    List<Employee> retrieveEmployeeEmailIsNull();
+
+    @Query("SELECT e from Employee e where e.email is not null")
+    List<Employee> retrieveEmployeeEmailIsNotNull();
+
+    //Sorting is ASC order
+    @Query("SELECT e FROM Employee e order by e.salary")
+    List<Employee> retrieveEmployeeSalaryOrderAsc();
+
+    @Query("SELECT e FROM Employee e order by e.salary desc ")
+    List<Employee> retrieveEmployeeSalaryOrderDesc();
 }
