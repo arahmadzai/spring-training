1db768105c4a73a198bfee60b44059a0ba72b9e7
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-11-29 16:06:32 -0500

    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)

--- spring-16-REST-Jackson/src/main/java/com/cydeo/dto/UserDTO.java
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/UserDTO.java
@@ -2,6 +2,7 @@ package com.cydeo.dto;
 
 import com.cydeo.enums.UserRole;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Data
@@ -10,6 +11,8 @@ import lombok.*;
 public class UserDTO {
 
     private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String username;
     private UserRole role;
