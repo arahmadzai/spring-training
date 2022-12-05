78039740c174a92f1858870a5e96177222c6c025
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-12-05 16:27:47 -0500

    Spring core abstraction

--- spring-05-Core-Abstraction/src/main/java/com/cydeo/CommentApp.java
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/CommentApp.java
@@ -10,6 +10,7 @@ public class CommentApp {
     public static void main(String[] args) {
 
 
+        //we created our obj for training purposes, since we don't have UI or DB
         Comment comment = new Comment();
         comment.setAuthor("Abid");
         comment.setText("Allah Is One");
